dc16ed8266cb75669d3897b97fe7a5095ff3a3db
Alexandre <alexandresosi@gmail.com>
2020-10-13 15:32:47 -0300

    teste

--- src/test/java/hello/HelloWorldConfigurationTests.java
+++ src/test/java/hello/HelloWorldConfigurationTests.java
@@ -43,7 +43,7 @@ public class HelloWorldConfigurationTests {
 	@Test
 	public void testGreeting() throws Exception {
 		ResponseEntity<String> entity = restTemplate
-				.getForEntity("http://localhost:" + this.port + "/", String.class);
+				.getForEntity("https://localhost:" + this.port + "/", String.class);
 		assertEquals(HttpStatus.OK, entity.getStatusCode());
 	}
 
